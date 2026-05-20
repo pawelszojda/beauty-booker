b35495ec26b930e80ab3bc44e5dec1571e5c415d
Paweł Szojda <pawelszojda@icloud.com>
2026-05-20 11:35:35 +0200

    Create migrations

---
+++ database/migrations/2026_05_20_084117_create_appointments_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use App\Models\Customer;
+use App\Models\Service;
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('appointments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Customer::class)->constrained();
+            $table->foreignIdFor(Service::class)->constrained();
+            $table->foreignIdFor(User::class)->constrained();
+            $table->dateTime('start_time');
+            $table->dateTime('end_time');
+            $table->string('status')->default('oczekująca');
+            $table->string('google_event_id')->nullable()->index();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('appointments');
+    }
+};
