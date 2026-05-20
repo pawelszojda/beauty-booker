c8585ef15cb8a05c79e48f0809013ac17e7db1f5
Paweł Szojda <pawelszojda@icloud.com>
2026-05-20 21:56:26 +0200

    Add i18n package to use translations

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,6 +4,7 @@ import './bootstrap';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createApp, h } from 'vue';
+import { i18nVue } from 'laravel-vue-i18n';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
@@ -18,6 +19,13 @@ createInertiaApp({
     setup({ el, App, props, plugin }) {
         return createApp({ render: () => h(App, props) })
             .use(plugin)
+            .use(i18nVue, {
+                resolve: async (lang) => {
+                    const langs = import.meta.glob('../../lang/*.json');
+
+                    return await langs[`../../lang/${lang}.json`]();
+                },
+            })
             .use(ZiggyVue)
             .mount(el);
     },
